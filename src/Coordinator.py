3797c8f9ed7409526dee451d8259cdcfd0dd1c3f
will <will.andrew@bristol.ac.uk>
2017-08-16 15:58:45 +0100

    Added tensorflow/image callback integration

--- src/Coordinator.py
+++ src/Coordinator.py
@@ -3,9 +3,37 @@
 roslib.load_manifest('uav_id')
 import rospy as ros
 
+# Constant definitions
+
+# Possible UAV states
+states = {	
+			'GROUNDED': 0, 
+			'TAKEOFF': 1, 
+			'HOVERING': 2, 
+			'MOVE': 3, 
+			'EXPERIMENT': 4, 
+			'LANDING': 5
+		 }
+
 class Coordinator:
 	def __init__(self):
+		self._current_state = states['GROUNDED']
 
+	def coordinate(self):
+		if self._current_state == states['GROUNDED']:
+
+		elif self._current_state == states['TAKEOFF']:
+
+		elif self._current_state == states['HOVERING']:
+
+		elif self._current_state == states['MOVE']:
+
+		elif self._current_state == states['EXPERIMENT']:
+
+		elif self._current_state == states['LANDING']:
+
+		else:
+			ros.logerr("State not recognised")
 
 # Entry method
 if __name__ == '__main__':
--- src/ImageProcessor.py
+++ src/ImageProcessor.py
@@ -6,36 +6,72 @@ roslib.load_manifest('uav_id')
 import rospy as ros
 import numpy as np
 
+import tensorflow as tf
+
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class ImageProcessor:
 	def __init__(self):
 		# Subscriber and respective callback functions
-		sub_image = ros.Subscriber('image', Image, self.imageCallback)
+		sub_image = ros.Subscriber('cam_img', Image, self.imageCallback)
 
 		# Publishers
+
+		# ROS parameters (from launch file or otherwise)
+		self._v3_network_loc = ros.get_param("~v3-network-loc")
 
 		# Class attributes
 		self._bridge = CvBridge()
 		self._window_name = "ImageProcessor"
 
+		# Tensorflow attributes/initialisation
+		with tf.gfile.FastGFile(self._v3_network_loc, 'rb') as f:
+			graph_def = tf.GraphDef()
+			graph_def.ParseFromString(f.read())
+			_ = tf.import_graph_def(graph_def, name='')
+			ros.loginfo("Loaded network at directory: %s", self._v3_network_loc)
+
+		self._tf_sesh = tf.Session()
+		self._pool_tensor = self._tf_sesh.graph.get_tensor_by_name('pool_3:0')
+
+	# Called each time an image from the camera is received
 	def imageCallback(self, image_msg):
+		# Try converting the image into OpenCV format from ROS
 		try:
 			cv_image = self._bridge.imgmsg_to_cv2(image_msg, "bgr8")
 		except CvBridgeError as e:
 			print e
 
+		# Extract Inception v3 convolutional representation of the input image
+		v3_out = self.extractV3Pool(cv_image)
+		print v3_out
+		print v3_out.shape
+
+		# Display the image
 		cv2.imshow(self._window_name, cv_image)
 		cv2.waitKey(3)
 
+	# Given an OpenCV image, extract its Inception V3 feature representation
+	def extractV3Pool(self, image):
+		# Change the image resolution, change into TF-style image
+		image = cv2.resize(image, (299, 299), interpolation=cv2.INTER_CUBIC)
+		tf_image = np.asarray(image)
+		tf_image = np.expand_dims(tf_image, axis=0)
+
+		# Get the image's pool3 CNN representation
+		cnn_representation = self._tf_sesh.run(self._pool_tensor,
+			{'Mul:0': tf_image})
+
+		return cnn_representation
+
 # Entry method
 if __name__ == '__main__':
-	# Create a ImageProcessor object instance
-	ip = ImageProcessor()
-
 	# Initialise this node
 	ros.init_node('image_processor')
+
+	# Create a ImageProcessor object instance
+	ip = ImageProcessor()
 
 	# Spin away
 	try:
