649d6df358b72e5dbcf9e85bd816f06368ba97d1
will <will.andrew@bristol.ac.uk>
2017-09-06 10:05:07 +0100

    Grid camera is now fully a class

--- src/GridCameraController.py
+++ src/GridCameraController.py
@@ -5,77 +5,102 @@ roslib.load_manifest('uav_id')
 import rospy as ros
 from gazebo_msgs.msg import *
 from std_srvs.srv import Empty
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-rob_name = "sim_cam"
-current_pose = ModelState()
 
-# Position movement vectors [x,y,z]
-forward	= []
-backward = []
-left = []
-right = []
-up = []
-down = []
+class GridCameraController:
+	def __init__(self):
+		### ROS Publishers
+		self._pos_pub = ros.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
 
-def updateMovementVectors(gran):
-	forward		= [gran,	0,		0]
-	backward	= [-gran,	0,		0]
-	left		= [0,	 gran,		0]
-	right		= [0,	-gran,		0]
-	up			= [0,		0,	 gran]
-	down		= [0,		0,	-gran]
+		### ROS Subscribers
+		self._pos_sub = ros.Subscriber('/gazebo/model_states', ModelStates, self.poseCallback)
+		self._sub_image = ros.Subscriber('cam_img', Image, self.imageCallback)
 
-def move(move_input):
-	#enableDisablePhysics(True)
-	desired_pose = current_pose
-	desired_pose.pose.position.x = current_pose.pose.position.x + move_input[0]
-	desired_pose.pose.position.y = current_pose.pose.position.y + move_input[1]
-	desired_pose.pose.position.z = current_pose.pose.position.z + move_input[2]
-	pos_pub.publish(desired_pose)
-	#enableDisablePhysics(False)
+		### ROS Parameters
+		self._granularity = ros.get_param("~granularity", 1)
 
-def poseCallback(data):
-	try:
-		idx = data.name.index(rob_name)
-		current_pose.model_name = data.name[idx]
-		current_pose.pose = data.pose[idx]
-		current_pose.twist = data.twist[idx]
-		#print current_pose
-	except:
-		pass
+		### Class attributes
+		# Movement vectors
+		self._forward	= []
+		self._backward 	= []
+		self._left 		= []
+		self._right 	= []
+		self._up 		= []
+		self._down 		= []
+
+		# Store the current object pose
+		self._current_pose = ModelState()
+
+		# Model robot name from gazebo/launch file/xacro definition
+		self._rob_name = "sim_cam"
+
+		### Pre-processing
+		self.updateMovementVectors(self._granularity)
+
+	def updateMovementVectors(self, gran):
+		self._forward	= [gran,	0,		0]
+		self._backward	= [-gran,	0,		0]
+		self._left		= [0,	 gran,		0]
+		self._right		= [0,	-gran,		0]
+		self._up		= [0,		0,	 gran]
+		self._down		= [0,		0,	-gran]
+
+	def poseCallback(self, data):
+		try:
+			idx = data.name.index(self._rob_name)
+			self._current_pose.model_name = data.name[idx]
+			self._current_pose.pose = data.pose[idx]
+			self._current_pose.twist = data.twist[idx]
+			#print current_pose
+		except:
+			pass
+
+	# Callback function for attached simulated camera
+	def imageCallback(self, image_msg):
+		# Try converting the image into OpenCV format from ROS
+		try:
+			cv_image = self._bridge.imgmsg_to_cv2(image_msg, "bgr8")
+		except CvBridgeError as e:
+			print e
+
+		# Do something with the image
+
+	def move(self, move_input):
+		desired_pose = self._current_pose
+		desired_pose.pose.position.x = self._current_pose.pose.position.x + move_input[0]
+		desired_pose.pose.position.y = self._current_pose.pose.position.y + move_input[1]
+		desired_pose.pose.position.z = self._current_pose.pose.position.z + move_input[2]
+		self._pos_pub.publish(desired_pose)
 
 # Entry method
 if __name__ == '__main__':
 	# Initialise this ROS node
 	ros.init_node('grid_camera_controller')
 
-	# Publishers
-	pos_pub = ros.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
+	# Create an object instance
+	gcc = GridCameraController()
 
-	# Subscribers
-	pos_sub = ros.Subscriber('/gazebo/model_states', ModelStates, poseCallback)
-
-	# Parameters
-	granularity = ros.get_param("~granularity", 1)
-	updateMovementVectors(granularity)
+	ros.sleep(4)
 
 	# Main loop
 	try:
 		while not ros.is_shutdown():
 			ros.loginfo("Moving forward")
-			move(forward)
+			gcc.move(gcc._forward)
 			ros.sleep(2)
 
 			ros.loginfo("Moving backward")
-			move(backward)
+			gcc.move(gcc._backward)
 			ros.sleep(2)
 
 			ros.loginfo("Moving up")
-			move(up)
+			gcc.move(gcc._up)
 			ros.sleep(2)
 
 			ros.loginfo("Moving down")
-			move(down)
+			gcc.move(gcc._down)
 			ros.sleep(2)
 
 	except ros.ROSInterruptException:
